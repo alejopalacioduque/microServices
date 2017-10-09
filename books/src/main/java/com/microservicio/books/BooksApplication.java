ad46b848d17de1376c873ae39bf86a66e5174964
Alejandro Palacio <alejopalacioduque@gmail.com>
2017-10-09 01:58:22 -0500

    server config , Eureka , book service

---
+++ books/src/main/java/com/microservicio/books/BooksApplication.java
@@ -0,0 +1,14 @@
+package com.microservicio.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class BooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApplication.class, args);
+	}
+}
